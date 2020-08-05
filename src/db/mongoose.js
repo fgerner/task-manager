3e4418666dbb0a0f8826b31dc8659fb0a1842407
Fred <fred.gerner@icloud.com>
2020-08-05 22:46:27 +1000

    added endpoint for post tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,40 +1,7 @@
 const mongoose = require('mongoose');
 
-const User = require('../models/users');
-
-const faker = require('faker');
-
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-const Task = mongoose.model('Task', {
-    description: {type: String, required: true, trim: true},
-    completed: {type: Boolean, default: false}
-});
-
-// const task = new Task({
-//     description: faker.lorem.sentence(),
-//     completed: faker.random.boolean()
-// })
-//
-// task.save().then(() => {
-//     console.log(task);
-// }).catch((error) => {
-//     console.log(error);
-// })
-
-// const me = new User({
-//     name: faker.name.firstName(),
-//     password: faker.lorem.words(2),
-//     email: faker.internet.email(),
-//     age: Math.floor(Math.random() * 79 + 21)
-// })
-//
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log(error);
-// });
