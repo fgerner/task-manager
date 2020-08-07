d4987e707c4e8a343af92169e2bfdeca622de376
Fred <fred.gerner@icloud.com>
2020-08-07 20:39:56 +1000

    added endpoint for get users and tasks

--- src/index.js
+++ src/index.js
@@ -17,6 +17,30 @@ app.post('/users', (req, res) => {
         res.statusCode = 400;
         res.send(e);
     });
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.statusCode = 500;
+        res.send(e);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+        if (!user) {
+            res.statusCode = 404;
+            return res.send('User not found');
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.statusCode = 500;
+        res.send(e);
+    })
+
 })
 
 app.post('/tasks', (req, res) => {
@@ -28,6 +52,28 @@ app.post('/tasks', (req, res) => {
         res.statusCode = 400;
         res.send(e);
     });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find().then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.statusCode = 500;
+        res.send(e);
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if (!task) {
+            res.statusCode = 404;
+            return res.send('Task not found.');
+        }
+        res.send(task);
+    }).catch((e) => {
+        res.statusCode = 500;
+        res.send(e);
+    })
 })
 
 app.listen(port, () => {
