df8ce1328e8cdcd50586d4dfe8167fc3edc8449a
Fred <fred.gerner@icloud.com>
2020-08-08 21:13:09 +1000

    refactored user routes

--- src/index.js
+++ src/index.js
@@ -2,85 +2,14 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const userRoutes = require('./routes/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(userRoutes);
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save();
-        res.statusCode = 201;
-        res.send(user);
-    } catch (e) {
-        res.statusCode = 400;
-        res.send(e);
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.statusCode = 500;
-        res.send(e);
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            res.statusCode = 404;
-            return res.send('User not found.');
-        }
-        res.send(user);
-    } catch (e) {
-        res.statusCode = 500;
-        res.send(e);
-    }
-});
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ["name", "email", "password", "age"];
-    const isAllowedOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isAllowedOperation) {
-        res.statusCode = 400;
-        return res.send({error: 'Not a valid update'})
-    }
-    const _id = req.params.id;
-    try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
-        if (!user) {
-            res.statusCode = 404;
-            return res.send('User not found.');
-        }
-        res.send(user);
-    } catch (e) {
-        res.statusCode = 400;
-        res.send(e);
-    }
-});
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-            res.statusCode = 404;
-            return res.send('User not found.');
-        }
-        res.send(user);
-    } catch (e) {
-        res.statusCode = 500;
-        res.send(e);
-    }
-})
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
---
+++ src/routes/user.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save();
+        res.statusCode = 201;
+        res.send(user);
+    } catch (e) {
+        res.statusCode = 400;
+        res.send(e);
+    }
+});
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (e) {
+        res.statusCode = 500;
+        res.send(e);
+    }
+});
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            res.statusCode = 404;
+            return res.send('User not found.');
+        }
+        res.send(user);
+    } catch (e) {
+        res.statusCode = 500;
+        res.send(e);
+    }
+});
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isAllowedOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isAllowedOperation) {
+        res.statusCode = 400;
+        return res.send({error: 'Not a valid update'})
+    }
+    const _id = req.params.id;
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        if (!user) {
+            res.statusCode = 404;
+            return res.send('User not found.');
+        }
+        res.send(user);
+    } catch (e) {
+        res.statusCode = 400;
+        res.send(e);
+    }
+});
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            res.statusCode = 404;
+            return res.send('User not found.');
+        }
+        res.send(user);
+    } catch (e) {
+        res.statusCode = 500;
+        res.send(e);
+    }
+});
+
+module.exports = router;
