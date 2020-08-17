08c3e165d7ed5cf6e7009c2d06f8260f82e57dc0
Fred <fred.gerner@icloud.com>
2020-08-17 20:05:03 +1000

    added endpoint for deleting avatar

--- src/routes/user.js
+++ src/routes/user.js
@@ -94,6 +94,11 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.send({error: error.message});
 });
 
+router.delete('/users/me/avatar', auth, async (req, res) => {
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send();
+});
 
 
 module.exports = router;
