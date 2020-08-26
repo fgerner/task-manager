cb801d18f746303441d9a3ea110c274411b9fb90
Fred <fred.gerner@icloud.com>
2020-08-26 19:13:18 +1000

    added more tests

--- src/routes/task.test.js
+++ src/routes/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 const Task = require('../models/task');
-const {userOne, userTwo, taskOne, taskTwo, setupData} = require('./fixtures/db');
+const {userOne, userTwo, taskOne, taskTwo, taskThree, setupData} = require('./fixtures/db');
 
 beforeEach(setupData);
 
@@ -26,6 +26,13 @@ describe('task', () => {
             .expect(200);
         expect(response.body.length).toEqual(2);
     });
+    it('should not get other users tasks', async () => {
+        await request(app)
+            .get('/tasks/' + taskThree._id)
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(404);
+    });
     it('should not delete other users tasks', async () => {
         await request(app)
             .delete('/tasks/' + taskOne._id)
@@ -46,12 +53,12 @@ describe('task', () => {
         expect(task.id).toEqual(taskTwo.id);
     });
     it('should not delete task if unauthenticated', async () => {
-            await request(app)
-                .delete('/tasks/' + taskOne._id)
-                .send()
-                .expect(401);
-            const task = await Task.find({owner: userOne._id});
-            expect(task.length).toEqual(2);
-        });
+        await request(app)
+            .delete('/tasks/' + taskOne._id)
+            .send()
+            .expect(401);
+        const task = await Task.find({owner: userOne._id});
+        expect(task.length).toEqual(2);
+    });
 
 });
